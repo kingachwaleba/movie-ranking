e7185fd23765264ebc82aa6d147ad68c286400fc
kingachwaleba <kingach6@gmail.com>
2021-03-03 13:00:06 +0100

    Added some fields to the user model

--- src/main/java/com/movieranking/backend/user/models/User.java
+++ src/main/java/com/movieranking/backend/user/models/User.java
@@ -1,4 +1,26 @@
 package com.movieranking.backend.user.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    @Size(min = 5, max = 20)
+    @NotBlank(message = "Login is mandatory!")
+    private String login;
+
+    @Column(unique = true, nullable = false, length = 20)
+    @Size(min = 5, max = 20)
+    @NotBlank(message = "Email is mandatory!")
+    private String email;
+
+    @Size(min = 5, max = 100)
+    private String specialNotes;
 }

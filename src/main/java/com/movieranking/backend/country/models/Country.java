b9ee59da30ce56e33ad5465b576ff54bcd31c279
kingachwaleba <kingach6@gmail.com>
2021-03-03 13:22:03 +0100

    Created the country model

--- src/main/java/com/movieranking/backend/country/models/Country.java
+++ src/main/java/com/movieranking/backend/country/models/Country.java
@@ -1,4 +1,14 @@
 package com.movieranking.backend.country.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String name;
 }

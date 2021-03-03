2cf68aca4c3e7b4e1a0f118ba2465065a048b986
kingachwaleba <kingach6@gmail.com>
2021-03-03 13:22:20 +0100

    Created the gender model

--- src/main/java/com/movieranking/backend/gender/models/Gender.java
+++ src/main/java/com/movieranking/backend/gender/models/Gender.java
@@ -1,4 +1,14 @@
 package com.movieranking.backend.gender.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Gender {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String name;
 }

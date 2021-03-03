853291d7a3ae49a53d601c992484b151264e84de
kingachwaleba <kingach6@gmail.com>
2021-03-03 13:33:00 +0100

    Created the genre model

--- src/main/java/com/movieranking/backend/genre/models/Genre.java
+++ src/main/java/com/movieranking/backend/genre/models/Genre.java
@@ -1,4 +1,14 @@
 package com.movieranking.backend.genre.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String name;
 }

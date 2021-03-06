97323d1ec668b3fc017ffde3ab9ca2893bb5214e
kingachwaleba <kingach6@gmail.com>
2021-03-06 19:47:08 +0100

    Created the movie model

--- src/main/java/com/movieranking/backend/movie/models/Movie.java
+++ src/main/java/com/movieranking/backend/movie/models/Movie.java
@@ -1,4 +1,27 @@
 package com.movieranking.backend.movie.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    @Size(min = 1, max = 50)
+    @NotBlank(message = "Name is mandatory!")
+    private String name;
+
+    @Column(length = 1000)
+    @Size(min = 50, max = 1000)
+    private String description;
+
+    @Column(nullable = false, length = 4)
+    @Size(min = 4, max = 4)
+    @NotBlank(message = "Year is mandatory!")
+    private String year;
 }

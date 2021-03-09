6b8b0fd50c387ae650ae1cb143e90ea04f799143
kingachwaleba <kingach6@gmail.com>
2021-03-10 00:05:51 +0100

    Tried to created a sample home page

--- src/main/java/com/movieranking/backend/movie/controllers/MovieController.java
+++ src/main/java/com/movieranking/backend/movie/controllers/MovieController.java
@@ -1,4 +1,24 @@
 package com.movieranking.backend.movie.controllers;
 
+import com.movieranking.backend.movie.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MovieController {
+
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @GetMapping("/index")
+    public String showMovies(Model model) {
+        model.addAttribute("movies", movieRepository.findAll());
+        return "main-layout";
+    }
 }

248997b93657f198cf94ca342c2e37a63699f1cf
kingachwaleba <kingach6@gmail.com>
2021-03-10 19:54:47 +0100

    Provides service for registering account

--- src/main/java/com/movieranking/backend/user/services/UserService.java
+++ src/main/java/com/movieranking/backend/user/services/UserService.java
@@ -1,4 +1,10 @@
 package com.movieranking.backend.user.services;
 
+import com.movieranking.backend.user.models.User;
+
 public interface UserService {
+
+    void save(User user);
+
+    User findByLogin(String login);
 }
--- src/main/java/com/movieranking/backend/user/services/UserServiceImpl.java
+++ src/main/java/com/movieranking/backend/user/services/UserServiceImpl.java
@@ -1,4 +1,38 @@
 package com.movieranking.backend.user.services;
 
-public class UserServiceImpl {
+import com.movieranking.backend.role.repositories.RoleRepository;
+import com.movieranking.backend.user.models.User;
+import com.movieranking.backend.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        userRepository.save(user)
+    }
+
+    @Override
+    public User findByLogin(String login) {
+        return userRepository.findByLogin(login);
+    }
 }

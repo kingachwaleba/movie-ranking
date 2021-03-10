8118a77f99cb7e99b76bbe9f10738c28fa8e65da
kingachwaleba <kingach6@gmail.com>
2021-03-10 19:44:18 +0100

    Fixded issues
    Fixed 'Could not autowire, no beans of
    authenticationmanager/userdetailsservice type found' issue.

--- src/main/java/com/movieranking/backend/config/WebSecurityConfig.java
+++ src/main/java/com/movieranking/backend/config/WebSecurityConfig.java
@@ -1,4 +1,24 @@
 package com.movieranking.backend.config;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
 }

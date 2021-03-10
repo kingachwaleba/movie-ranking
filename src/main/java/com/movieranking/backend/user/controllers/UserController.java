e420e6dbde83b05755440ead840ec5f87d6b20e5
kingachwaleba <kingach6@gmail.com>
2021-03-10 18:38:23 +0100

    Edited the user controller

--- src/main/java/com/movieranking/backend/user/controllers/UserController.java
+++ src/main/java/com/movieranking/backend/user/controllers/UserController.java
@@ -1,4 +1,16 @@
 package com.movieranking.backend.user.controllers;
 
+import com.movieranking.backend.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }

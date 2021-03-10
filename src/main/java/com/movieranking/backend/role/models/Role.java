9d8bfcbc67f36c6ad9bdfeec473da59a047167cf
kingachwaleba <kingach6@gmail.com>
2021-03-10 18:51:29 +0100

    Edited the role model

--- src/main/java/com/movieranking/backend/role/models/Role.java
+++ src/main/java/com/movieranking/backend/role/models/Role.java
@@ -1,4 +1,44 @@
 package com.movieranking.backend.role.models;
 
+import com.movieranking.backend.user.models.User;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String name;
+
+    @ManyToMany(mappedBy = "role")
+    private List<User> users;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }

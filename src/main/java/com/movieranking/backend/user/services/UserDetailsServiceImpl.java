a6c1314e614f4c6fad74f6ebd584484c97c0a4cf
kingachwaleba <kingach6@gmail.com>
2021-03-10 19:26:15 +0100

    Implemented login/authentication

--- src/main/java/com/movieranking/backend/user/services/UserDetailsServiceImpl.java
+++ src/main/java/com/movieranking/backend/user/services/UserDetailsServiceImpl.java
@@ -1,4 +1,39 @@
 package com.movieranking.backend.user.services;
 
-public class UserDetailsServiceImpl {
+import com.movieranking.backend.role.models.Role;
+import com.movieranking.backend.user.models.User;
+import com.movieranking.backend.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String login) {
+        User user = userRepository.findByLogin(login);
+
+        if (user == null) throw new UsernameNotFoundException(login);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+        for (Role role : user.getRoles()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), grantedAuthorities);
+    }
 }

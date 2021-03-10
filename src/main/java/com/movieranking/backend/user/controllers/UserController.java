601ebe36ee351ceb2f99f80d288199ff2db93d8c
kingachwaleba <kingach6@gmail.com>
2021-03-11 00:13:46 +0100

    Edited user controller

--- src/main/java/com/movieranking/backend/user/controllers/UserController.java
+++ src/main/java/com/movieranking/backend/user/controllers/UserController.java
@@ -1,16 +1,60 @@
 package com.movieranking.backend.user.controllers;
 
-import com.movieranking.backend.user.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.movieranking.backend.user.models.User;
+import com.movieranking.backend.user.services.SecurityService;
+import com.movieranking.backend.user.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    @Autowired
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    private final SecurityService securityService;
+
+    public UserController(UserService userService, SecurityService securityService) {
+        this.userService = userService;
+        this.securityService = securityService;
+    }
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        userService.save(userForm);
+
+        securityService.autoLogin(userForm.getLogin(), userForm.getPasswordConfirm());
+
+        return "redirect:/index";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+    @GetMapping({"/", "/index"})
+    public String welcome(Model model) {
+        return "welcome";
     }
 }

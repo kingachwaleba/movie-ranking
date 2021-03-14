c77d2a18dbd0a2c353703de43ac64071a2ffa88a
kingachwaleba <kingach6@gmail.com>
2021-03-14 15:11:33 +0100

    Redirected a logged-in user from login form

--- src/main/java/com/movieranking/backend/user/controllers/UserController.java
+++ src/main/java/com/movieranking/backend/user/controllers/UserController.java
@@ -48,6 +48,10 @@ public class UserController {
 
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
+        if (securityService.isAuthenticated()) {
+            return "redirect:/";
+        }
+
         if (error != null)
             model.addAttribute("error", "Your username and password is invalid.");
 

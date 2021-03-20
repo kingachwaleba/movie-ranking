096ff7e3013eb24778b1977c05bca87638498543
kingachwaleba <kingach6@gmail.com>
2021-03-20 15:37:15 +0100

    Added getters and setters

--- src/main/java/com/movieranking/backend/movie/models/Movie.java
+++ src/main/java/com/movieranking/backend/movie/models/Movie.java
@@ -24,4 +24,36 @@ public class Movie {
     @Size(min = 4, max = 4)
     @NotBlank(message = "Year is mandatory!")
     private String year;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 }

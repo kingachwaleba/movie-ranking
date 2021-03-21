042e57344701487f3f9352b521dd869532e73b99
kingachwaleba <kingach6@gmail.com>
2021-03-21 16:21:59 +0100

    Edited get method and add post method

--- src/main/java/com/movieranking/backend/movie/controllers/MovieController.java
+++ src/main/java/com/movieranking/backend/movie/controllers/MovieController.java
@@ -1,10 +1,15 @@
 package com.movieranking.backend.movie.controllers;
 
+import com.movieranking.backend.movie.models.Movie;
 import com.movieranking.backend.movie.repositories.MovieRepository;
+import com.movieranking.backend.user.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MovieController {
@@ -17,8 +22,15 @@ public class MovieController {
     }
 
     @GetMapping("/add-movie")
-    public String showMovies(Model model) {
-        model.addAttribute("movies", movieRepository.findAll());
+    public String addMovie(Model model) {
+
+        model.addAttribute("movieForm", new Movie());
+
         return "views/add-movie";
+    }
+
+    @PostMapping("/add-movie")
+    public String addMovie(@ModelAttribute("movieForm") Movie movieForm, BindingResult bindingResult) {
+        return null;
     }
 }

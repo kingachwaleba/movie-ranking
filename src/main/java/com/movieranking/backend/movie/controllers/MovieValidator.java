8b325062f007e4d538492dd4034de1c21cc3732d
kingachwaleba <kingach6@gmail.com>
2021-03-21 20:42:23 +0100

    Provided input-data validation for movie form

--- src/main/java/com/movieranking/backend/movie/controllers/MovieValidator.java
+++ src/main/java/com/movieranking/backend/movie/controllers/MovieValidator.java
@@ -1,4 +1,51 @@
 package com.movieranking.backend.movie.controllers;
 
-public class MovieValidator {
+import com.movieranking.backend.movie.models.Movie;
+import com.movieranking.backend.movie.services.MovieService;
+import org.springframework.stereotype.Component;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class MovieValidator implements Validator {
+
+    private final MovieService movieService;
+
+    public MovieValidator(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Movie.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Movie movie = (Movie) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty");
+        if (movie.getName().length() > 51) {
+            errors.rejectValue("name", "Size.movieForm.name");
+        }
+//        if (movieService.findByName(user.getName()) != null) {
+//            errors.rejectValue("name", "Duplicate.movieForm.name");
+//        }
+
+        if (movie.getDescription().length() != 0 && movie.getDescription().length() > 1000) {
+            errors.rejectValue("description", "Size.movieForm.description");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "year", "NotEmpty");
+        int year = Integer.getInteger(movie.getYear());
+        if (year < 1850 || year > 2100) {
+            errors.rejectValue("year", "Size.movieForm.year");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "country", "NotEmpty");
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "genre", "NotEmpty");
+    }
 }
